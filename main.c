67892aceb9461deadc250e0154ca9b2ced98c154
RFerreira014 <speranikta@hotmail.com>
2026-06-30 19:24:02 +0000

    string

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char nome[] = "Rui Ferreira";
+    char idade[] = "tenho 50";
+    char email[] = "speranikta@hotmail.com";
+
+    printf("O meu nome é %s\n", nome);
+    printf("A minha idade é %s anos\n", idade);
+    printf("O meu email é %s\n", email);
+
+    return 0;
+}
